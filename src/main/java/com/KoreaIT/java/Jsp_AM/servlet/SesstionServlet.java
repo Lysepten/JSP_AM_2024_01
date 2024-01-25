6a69486451abff0d40e873241539bb1295cd8b52
Lysepten <tlstlsrbrb11@gamail.com>
2024-01-25 13:28:27 +0900

    로그인,로그아웃 기능 구현

--- src/main/java/com/KoreaIT/java/Jsp_AM/servlet/SesstionServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/SesstionServlet.java
@@ -8,14 +8,14 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/member/join")
-public class MemberJoinServlet extends HttpServlet {
+@WebServlet("/sesstion/islogin")
+public class SesstionServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.getRequestDispatcher("/jsp/member/join.jsp").forward(request, response);
+		request.getRequestDispatcher("/jsp/sesstion/islogin.jsp").forward(request, response);
 	}
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
--- src/main/webapp/jsp/article/modify.jsp
+++ src/main/webapp/jsp/article/modify.jsp
@@ -17,7 +17,12 @@ Map<String, Object> articleRow = (Map<String, Object>) request.getAttribute("art
 
 	<h3><%=articleRow.get("id")%>번 게시물 수정
 	</h3>
-
+	<div>
+		번호 :
+		<%=articleRow.get("id")%></div>
+	<div>
+		날짜 :
+		<%=articleRow.get("regDate")%></div>
 	<form method="POST" action="doModify">
 		<input type="hidden" value="<%=articleRow.get("id")%>" name="id" />
 		<div>
@@ -30,6 +35,7 @@ Map<String, Object> articleRow = (Map<String, Object>) request.getAttribute("art
 		</div>
 		<button type="submit">수정</button>
 	</form>
+
 
 	<div>
 		<a style="color: green" href="list">리스트로 돌아가기</a>
